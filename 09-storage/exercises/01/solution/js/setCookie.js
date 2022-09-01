62a40f3af40c3517eb31c4c98205fb4948f6477d
szabi1112 <banki.szabolcs@gmail.com>
2022-09-01 20:28:23 +0200

    Nincs kész

--- 09-storage/exercises/01/solution/js/setCookie.js
+++ 09-storage/exercises/01/solution/js/setCookie.js
@@ -1,1 +1,8 @@
+//Írj egy `setCookie` nevű függvényt, ami a paraméterként kapott stringet eltárolja egy `token` nevű sütiben, ami 15 perc után lejár!
+
+const setCookie= function(string){
+    const expiry = new Date(Date.now()+1000*60*15).toUTCString
+    document.cookie=`token=${string}; expires=${expiry}; path=/`
+}
+
 export default setCookie;
